2be90d9c05128c16ddebdae699bd7267f40fb027
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-09-21 23:56:53 +0530

    fix comment

--- kairo-core/src/io.rs
+++ kairo-core/src/io.rs
@@ -1,6 +1,6 @@
 //Some input ouput shit here
 
-//Walk dir needed for recurssive reading markdowns and then catching them in a vector maybe,
+//Walk dir needed for recurssivee reading markdowns and then catching them in a vector maybe,
 // So basically vector of Paths of each markdown , ex: vec![PathBuf::from("path/to/file.md"), PathBuf::from("path/to/another.md")]
 
 use std::path::{Path, PathBuf};
